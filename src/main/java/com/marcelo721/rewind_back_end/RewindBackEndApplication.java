219763ff756e7d20ade88f92e95f909738ebba58
Marcelo Henrique <mh0473569@gmail.com>
2026-01-30 11:43:20 -0300

    first commit

---
+++ src/main/java/com/marcelo721/rewind_back_end/RewindBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo721.rewind_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewindBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewindBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelo721/rewind_back_end/RewindBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo721.rewind_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewindBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
