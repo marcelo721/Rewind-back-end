e8226d88269663546a0d92f7f06e54e35ad6a07e
marcelo721 <marceloh.sousa@alu.ufc.br>
2026-04-06 18:30:45 -0300

    feat: created BookDetails class and BookProviderRepository

--- src/main/java/com/marcelo721/rewind_back_end/Adapters/outBound/external/GoogleBooks/GoogleBookProviderImpl.java
+++ src/main/java/com/marcelo721/rewind_back_end/Adapters/outBound/external/GoogleBooks/GoogleBookProviderImpl.java
@@ -1,7 +1,20 @@
 package com.marcelo721.rewind_back_end.Adapters.outBound.external.GoogleBooks;
 
+import com.marcelo721.rewind_back_end.domain.model.entities.MovieDetails;
+import com.marcelo721.rewind_back_end.domain.model.entities.MovieSummary;
 import com.marcelo721.rewind_back_end.domain.ports.BookProviderRepository;
 
+import java.util.List;
+
 public class GoogleBookProviderImpl implements BookProviderRepository {
+    @Override
+    public MovieDetails FindById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<MovieSummary> searchByTitle(String title) {
+        return List.of();
+    }
 }
  
--- src/main/java/com/marcelo721/rewind_back_end/domain/ports/BookProviderRepository.java
+++ src/main/java/com/marcelo721/rewind_back_end/domain/ports/BookProviderRepository.java
@@ -1,4 +1,11 @@
 package com.marcelo721.rewind_back_end.domain.ports;
 
+import com.marcelo721.rewind_back_end.domain.model.entities.MovieDetails;
+import com.marcelo721.rewind_back_end.domain.model.entities.MovieSummary;
+
+import java.util.List;
+
 public interface BookProviderRepository {
+    MovieDetails FindById(String id);
+    List<MovieSummary> searchByTitle(String title);
 }
