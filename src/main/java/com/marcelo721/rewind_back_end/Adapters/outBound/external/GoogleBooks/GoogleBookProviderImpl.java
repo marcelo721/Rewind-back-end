fbc7de9975e43dcc6a1295f307411f21770cb13d
marcelo721 <marceloh.sousa@alu.ufc.br>
2026-04-06 18:26:22 -0300

    feat: created BookDetails class and BookProviderReposotpru

---
+++ src/main/java/com/marcelo721/rewind_back_end/Adapters/outBound/external/GoogleBooks/GoogleBookProviderImpl.java
@@ -0,0 +1,7 @@
+package com.marcelo721.rewind_back_end.Adapters.outBound.external.GoogleBooks;
+
+import com.marcelo721.rewind_back_end.domain.ports.BookProviderRepository;
+
+public class GoogleBookProviderImpl implements BookProviderRepository {
+}
+ 
---
+++ src/main/java/com/marcelo721/rewind_back_end/domain/ports/BookProviderRepository.java
@@ -0,0 +1,4 @@
+package com.marcelo721.rewind_back_end.domain.ports;
+
+public interface BookProviderRepository {
+}
