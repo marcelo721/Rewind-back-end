d86cf80ab9121d0a360bb8afe913f7361f53f691
marcelo721 <marceloh.sousa@alu.ufc.br>
2026-04-09 09:35:51 -0300

    feat(books): implement Google Books API integration

--- src/main/java/com/marcelo721/rewind_back_end/Adapters/inBoud/Controllers/BookController.java
+++ src/main/java/com/marcelo721/rewind_back_end/Adapters/inBoud/Controllers/BookController.java
@@ -1,4 +1,30 @@
 package com.marcelo721.rewind_back_end.Adapters.inBoud.Controllers;
 
+import com.marcelo721.rewind_back_end.Application.useCases.BookDetailsUseCases;
+import com.marcelo721.rewind_back_end.domain.model.entities.BookDetails;
+import com.marcelo721.rewind_back_end.domain.model.entities.BookSummary;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private final BookDetailsUseCases BookuseCases;
+
+    public BookController( BookDetailsUseCases bookService) {
+        this.BookuseCases = bookService;
+    }
+
+    @GetMapping
+    public List<BookSummary> searchBooks(@RequestParam String title) {
+        return BookuseCases.searchByTitle(title);
+    }
+
+    // 📖 Buscar detalhes por ID
+    @GetMapping("/{id}")
+    public BookDetails getBookById(@PathVariable String id) {
+        return BookuseCases.getFromProvider(id);
+    }
 }
